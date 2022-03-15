d129ff6f2a92f19fe41618a580182152c5a30cfd
Aysel Mirza <tu7pgums7@aztu.local>
2022-03-15 10:52:53 +0400

    Program

---
+++ ConsoleApp3/Models/Phone.cs.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp3.Models
+{
+    class Phone
+    {
+        public string name;
+        public string producer;
+        public int year;
+        public string version;
+        public string ram;
+
+
+        public Phone(string name, string producer)
+            { 
+            this.name = name;
+            this.producer = producer;
+             }
+        public Phone(string name, string producer, int year, string version): this (name,producer)
+            {
+            this.year = year;
+            this.version = version;
+            }
+
+        public Phone(string name, string producer, int year, string version, string ram) : this(name, producer, year, version)
+        {
+            this.ram = ram;
+        }
+         public string GetPhoneFullField()
+        {
+            return $"I have a Phone. It's name {this.name}. It's produces {this.producer}. It's year {this.year}. It's version {this.version}.";
+        }
+
+
+
+
+    }
+
+
+}
+
+
+       
+
+
+
+
+
+
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using ConsoleApp3.Models;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region object
+            Phone phone = new Phone ( ReadlineStr("please, enter the name"), ReadlineStr("please,enter the producer"),ReadLineInt("please, enter the year"), ReadlineStr("please,enter the version"), ReadlineStr("please,enter the ram"));
+
+            Console.WriteLine(phone.GetPhoneFullField());
+
+            #endregion
+        }
+
+           static string ReadlineStr(string message)
+        {
+            Console.WriteLine(message);
+            return Console.ReadLine();
+        }
+        static int ReadLineInt(string message) 
+        {
+        TryAgain:
+            string result = ReadlineStr(message);
+            try
+            {
+                return Convert.ToInt32(result);
+            }
+            catch (Exception)
+            {
+                Console.Clear();
+                Console.WriteLine("try again!");
+                goto TryAgain;
+            }
+
+
+
+
+        }
+
+
+
+    }      
+}           
